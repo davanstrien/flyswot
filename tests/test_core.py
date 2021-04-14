d29c0271ef257acc585db1a6308bff2bdfc2b726
Daniel van Strien <davanstrien@users.noreply.github.com>
2021-04-14 12:03:47 +0100

    Add core (#29)

---
+++ tests/test_core.py
@@ -0,0 +1,141 @@
+"""Tests for core module."""
+import os
+import typing
+from pathlib import Path
+from typing import Any
+from typing import List
+
+import pytest
+from hypothesis import strategies  # type: ignore
+from hypothesis.core import given  # type: ignore
+
+from flyswot import core
+
+# flake8: noqa
+# mypy: allow-untyped-defs
+
+int_values = strategies.integers()
+str_values = strategies.text()
+
+patterns_to_test: List[str] = [
+    "fse",
+    "fbspi",
+    "Rblefr",
+    "fblefv",
+    "fs001r",
+    "fs001v",
+    "f001r",
+    "f001v",
+    "f197ar",
+    "f197br",
+    "fse002av",
+    "fse002bv",
+    "fbrigr",
+    "fbrigv",
+]
+
+
+@pytest.fixture(scope="session", params=patterns_to_test)
+def image_files_pattern(tmpdir_factory, fname):
+    """Fixture for image file patterns"""
+    a_dir = tmpdir_factory.mktemp("image_dir")
+    for number in range(2000):
+        file = a_dir.join(f"file_{fname}_{number}.tif")
+        file.ensure()
+        file2 = a_dir.join(f"file_{number}.jpg")
+        file2.ensure()
+    return a_dir
+
+
+@pytest.mark.parametrize("fname", patterns_to_test)
+def test_filter(fname, tmpdir):
+    """It filters files from pattern"""
+    a_dir = tmpdir.mkdir("image_dir")
+    for number in range(50):
+        file = a_dir.join(f"file_{fname}_{number}.tif")
+        file.ensure()
+        file2 = a_dir.join(f"file_{number}.jpg")
+        file2.ensure()
+    for i in range(5):  # create 25 files in 5 subfolders
+        a_sub_dir = a_dir.mkdir(f"{i}_dir")
+        for i in range(5):
+            file = a_sub_dir.join(f"file_{fname}_{i}.tif")
+            file.ensure()
+    matches = core.get_image_files_from_pattern(
+        a_dir,
+        fname,
+    )
+    files = [f for f in Path(a_dir).rglob("**/*") if f.is_file()]
+    assert len(files) == (50 * 2) + 25
+    assert len(list(matches)) == 50 + 25
+
+
+@pytest.fixture()
+def image_files(tmpdir_factory):
+    """Fixture for image files"""
+    a_dir = tmpdir_factory.mktemp("image_dir")
+    for fname in range(2000):
+        file = a_dir.join(f"file_{fname}.tif")
+        file.ensure()
+    for fname in range(1000):
+        file = a_dir.join(f"file_{fname}.jpg")
+        file.ensure()
+    return a_dir
+
+
+def test_filter_to_preferred_ext(image_files):
+    """It filters to preferred extension"""
+    tiff_files = list(Path(image_files).glob("*.tif"))
+    assert len(tiff_files) == 2000
+    jpeg_files = list(Path(image_files).glob("*.jpg"))
+    assert len(jpeg_files) == 1000
+    image_files = [f for f in Path(image_files).iterdir()]
+    return_files = list(core.filter_to_preferred_ext(image_files, ".jpg"))
+    assert len(return_files) == 2000
+
+
+@given(strategies.lists(int_values, min_size=1))
+def test_signal_last(l):
+    """It returns last element in list when `is"""
+    *_, last = l
+    for is_last_element, var in core.signal_last(l):
+        if is_last_element:
+            assert var == last
+
+
+@typing.no_type_check
+def test_signal_last_raises_value_error_with_no_length():
+    """It raises value error when no length"""
+    with pytest.raises(ValueError):
+        items: List[Any] = []
+        for i in core.signal_last(items):
+            print(i)
+
+
+@given(
+    strategies.dictionaries(
+        keys=strategies.text(min_size=1), values=strategies.integers(), min_size=1
+    )
+)
+def test_signal_last_dictionary(d):
+    """It returns last element for dictionary"""
+    *_, last = d
+    for is_last_element, var in core.signal_last(d.items()):
+        if is_last_element:
+            key, value = var
+            assert key == last
+
+
+@given(
+    strategies.dictionaries(
+        keys=strategies.text(min_size=1), values=strategies.integers(), min_size=1
+    )
+)
+def test_signal_last_truth(d):
+    """It returns True for last element"""
+    dict_len = len(d)
+    for i, (is_last_element, var) in enumerate(core.signal_last(d.items()), start=1):
+        if i != dict_len:
+            assert is_last_element == False
+        if i == dict_len:
+            assert is_last_element == True
