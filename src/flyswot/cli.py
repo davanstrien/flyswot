8ea27ee275052102787eb0b019af950082b95fe2
davanstrien <davanstrien@gmail.com>
2021-04-22 20:22:28 +0100

    update cli

---
+++ src/flyswot/cli.py
@@ -0,0 +1,14 @@
+"""main flyswot cli entrypoint"""
+import typer
+
+from flyswot import inference
+from flyswot import models
+
+app = typer.Typer()
+app.add_typer(models.app, name="model")
+app.add_typer(inference.app, name="predict")
+
+
+def main():
+    """main flyswot cli entrypoint"""
+    app()
