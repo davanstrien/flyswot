9d42a94f5dec0373c798a2d25bc73aa2c0c847ca
davanstrien <davanstrien@gmail.com>
2021-04-23 13:11:25 +0100

    update model repo url to new flyswot models repo

--- src/flyswot/config.py
+++ src/flyswot/config.py
@@ -1,4 +1,4 @@
 """configuration"""
 
 APP_NAME: str = "flyswot"
-MODEL_REPO_URL: str = "https://api.github.com/repos/davanstrien/learn-onnx/releases"
+MODEL_REPO_URL: str = "https://api.github.com/repos/davanstrien/flyswot-models/releases"
