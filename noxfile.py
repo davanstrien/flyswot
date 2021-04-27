f6a17fc892e52264b7788d50418e442ada7401bb
davanstrien <davanstrien@gmail.com>
2021-04-27 18:17:57 +0100

    remove tests from mypy session scope

--- noxfile.py
+++ noxfile.py
@@ -105,7 +105,7 @@ def safety(session: Session) -> None:
 @session(python=python_versions)
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
-    args = session.posargs or ["src", "tests", "docs/conf.py"]
+    args = session.posargs or ["src", "docs/conf.py"]
     session.install(".")
     session.install("mypy", "pytest")
     session.run("mypy", *args)
