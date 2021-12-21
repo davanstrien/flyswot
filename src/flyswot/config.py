3d15616a6be81fe1845286174dc5eab1f6b88002
Daniel van Strien <davanstrien@gmail.com>
2021-12-21 15:44:46 +0000

    add temporary testing repository

--- src/flyswot/config.py
+++ src/flyswot/config.py
@@ -1,4 +1,4 @@
 """configuration"""
 
 APP_NAME: str = "flyswot"
-MODEL_REPO_URL: str = "https://api.github.com/repos/davanstrien/flyswot-models/releases"
+MODEL_REPO_ID: str = "davanstrien/flyswot-test"
