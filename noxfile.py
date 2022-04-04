e5a8a68b1fd5c7f1757652889c8ef03594894c16
Daniel van Strien <davanstrien@gmail.com>
2022-04-04 18:52:48 +0100

    bump minimum python in nox file

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,7 @@ from nox_poetry import session
 
 
 package = "flyswot"
-python_versions = ["3.9", "3.8", "3.7"]
+python_versions = ["3.10", "3.9", "3.8"]
 nox.options.sessions = (
     "pre-commit",
     "safety",
