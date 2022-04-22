db0c7abfa76b06c800724efc8f35d7f035348e22
Sourcery AI <>
2022-04-22 11:25:21 +0000

    'Refactored by Sourcery'

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -98,9 +98,6 @@ def test_multi_prediction_batch(confidence: float, label: str, imfile: Any):
     assert batch.batch_labels
     assert len(list(batch.batch_labels)) == 2
     assert hasattr(batch.batch_labels, "__next__")
-    for labels in batch.batch_labels:
-        for label in labels:
-            assert True
 
 
 FIXTURE_DIR = os.path.join(
