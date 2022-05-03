5e23a8651a8658b1650f5ec9f3985e24e732aed5
Daniel van Strien <davanstrien@gmail.com>
2022-05-03 18:15:18 +0100

    split cli and general inference

--- src/flyswot/cli_inference.py
+++ src/flyswot/cli_inference.py
@@ -4,12 +4,9 @@ import mimetypes
 import re
 import string
 import time
-from abc import ABC
-from abc import abstractmethod
 from collections import defaultdict
 from collections import OrderedDict
 from dataclasses import asdict
-from dataclasses import dataclass
 from datetime import datetime
 from datetime import timedelta
 from functools import singledispatch
@@ -22,11 +19,8 @@ from typing import Optional
 from typing import Tuple
 from typing import Union
 
-import numpy as np
-import onnxruntime as rt
 import PIL
 import typer
-from PIL import Image  # type: ignore
 from rich import print
 from rich.columns import Columns
 from rich.layout import Layout
@@ -43,82 +37,13 @@ from transformers import pipeline
 from flyswot import core
 from flyswot import models
 from flyswot.console import console
+from flyswot.inference import MultiLabelImagePredictionItem
+from flyswot.inference import MultiPredictionBatch
+from flyswot.inference import PredictionBatch
+from flyswot.inferencesession import InferenceSession
 from flyswot.logo import flyswot_logo
 
 app = typer.Typer()
-
-
-@dataclass
-class ImagePredictionArgmaxItem:
-    """Most confident predicted label for an item.
-
-    Attributes:
-        path: The Path to the image
-        predicted_label: The predicted label i.e. the argmax value for the prediction tensor
-        confidence: The confidence for `predicted_label` i.e. the max value for prediction tensor
-    """
-
-    path: Path
-    predicted_label: str
-    confidence: float
-
-    def __post_init__(self) -> Union[Path, None]:
-        """attempt to get absolute path"""
-        try:
-            self.path: Path = self.path.absolute()
-        except AttributeError:
-            pass
-
-
-@dataclass
-class MultiLabelImagePredictionItem:
-    """Multiple predictions for a single image"""
-
-    path: Path
-    predictions: List[Dict[float, str]]
-
-    def _get_top_labels(self) -> List[str]:
-        """Get top labels"""
-        top_labels = []
-        for prediction in self.predictions:
-            top_label = sorted(prediction.items(), reverse=True)[0][1]
-            top_labels.append(top_label)
-        return top_labels
-
-    # Post init that gets top prediction label from predictions
-    def __post_init__(self):
-        """Get top prediction label"""
-        self.predicted_labels = self._get_top_labels()
-
-
-@dataclass
-class PredictionBatch:
-    """Container for ImagePredictionItems"""
-
-    batch: List[ImagePredictionArgmaxItem]
-
-    def __post_init__(self):
-        """Returns a iterable of all predicted labels in batch"""
-        self.batch_labels: Iterable[str] = (item.predicted_label for item in self.batch)
-
-
-@dataclass
-class MultiPredictionBatch:
-    """Container for MultiLabelImagePredictionItems"""
-
-    batch: List[MultiLabelImagePredictionItem]
-
-    def _get_predicted_labels(self) -> Iterable:
-        """Returns a iterable of all predicted labels in batch"""
-        return (item.predicted_labels for item in self.batch)
-
-    def __post_init__(self):
-        """Returns a iterable of lists containing all predicted labels in batch"""
-        # self.batch_labels: Iterable = (
-        #     list(itertoolz.pluck(0, pred))
-        #     for pred in zip(*[o.predictions for o in self.batch])
-        # )
-        self.batch_labels = self._get_predicted_labels()
 
 
 image_extensions = {k for k, v in mimetypes.types_map.items() if v.startswith("image/")}
@@ -235,6 +160,7 @@ def predict_directory(
     print_inference_summary(
         str(delta), pattern, directory, csv_fname, image_formats, images_checked
     )
+    print(models.hub_model_link(model_id))
 
 
 def print_inference_summary(
@@ -444,107 +370,6 @@ def _(predictions: MultiPredictionBatch, csv_fpath: Path, top_n: int = 2) -> Non
             field_names = list(row.keys())
             writer = csv.DictWriter(csv_file, fieldnames=field_names)
             writer.writerow(row)
-
-
-class InferenceSession(ABC):
-    """Abstract class for inference sessions"""
-
-    @abstractmethod
-    def __init__(self, model: Path, vocab: Optional[List] = None):  # pragma: no cover
-        """Inference Sessions should init from a model file and vocab"""
-        self.model = model
-        self.vocab = vocab
-
-    @abstractmethod
-    def predict_image(self, image: Path):  # pragma: no cover
-        """Predict a single image"""
-        pass
-
-    @abstractmethod
-    def predict_batch(self, batch: Iterable[Path], bs: int):  # pragma: no cover
-        """Predict a batch"""
-        pass
-
-
-def softmax(x):
-    """return softmax of `x`"""
-    x = x.reshape(-1)
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=0)
-
-
-class OnnxInferenceSession(InferenceSession):  # pragma: no cover
-    """onnx inference session"""
-
-    def __init__(self, model: Path, vocab: Path):
-        """Create onnx session"""
-        self.model = model
-        self.session = rt.InferenceSession(str(model))
-
-        self.vocab = models.load_vocab(vocab)
-        self.vocab_mappings = [dict(enumerate(v)) for v in self.vocab]
-
-    def predict_image(
-        self, image: Path
-    ) -> Union[ImagePredictionArgmaxItem, MultiLabelImagePredictionItem]:
-        """Predict a single image"""
-        img = self._load_image(image)
-        output_names = [o.name for o in self.session.get_outputs()]
-        raw_result = self.session.run(output_names, {"image": img})
-        if len(self.vocab) < 2:
-            pred = self._postprocess(raw_result)
-            arg_max = int(np.array(pred).argmax())
-            predicted_label = self.vocab_mappings[0][arg_max]
-            confidence = float(np.array(pred).max())
-            return ImagePredictionArgmaxItem(image, predicted_label, confidence)
-        else:
-            prediction_dicts = []
-            for vocab_map, pred in zip(self.vocab_mappings, raw_result):
-                predictions = self._postprocess(pred)
-                prediction_dict = {
-                    float(prediction): vocab_map[i]
-                    for i, prediction in enumerate(predictions)
-                }
-                prediction_dicts.append(prediction_dict)
-        return MultiLabelImagePredictionItem(image, prediction_dicts)
-
-    def _preprocess(self, input_data: np.ndarray) -> np.ndarray:
-        # converts the input data into the float32 input for onnx
-        img_data = input_data.astype("float32")
-
-        # normalize
-        mean_vec = np.array([0.485, 0.456, 0.406])
-        stddev_vec = np.array([0.229, 0.224, 0.225])
-        norm_img_data = np.zeros(img_data.shape).astype("float32")
-        for i in range(img_data.shape[0]):
-            norm_img_data[i, :, :] = (
-                img_data[i, :, :] / 255 - mean_vec[i]
-            ) / stddev_vec[i]
-
-        # add batch channel
-        norm_img_data = norm_img_data.reshape(1, 3, 512, 512).astype("float32")
-        return norm_img_data
-
-    def _load_image(self, file: Path) -> np.ndarray:
-        """loads image and carries out preprocessing for inference"""
-        image = Image.open(file, mode="r")
-        image = image.resize((512, 512), Image.BILINEAR)
-        image_data = np.array(image).transpose(2, 0, 1)
-        return self._preprocess(image_data)
-
-    def _postprocess(self, result: List):
-        """process results from onnx session"""
-        return softmax(np.array(result)).tolist()
-
-    def predict_batch(
-        self, batch: Iterable[Path], bs: int
-    ) -> Union[PredictionBatch, MultiPredictionBatch]:
-        """predicts a batch of images"""
-        prediction_items = [self.predict_image(file) for file in batch]
-        if len(self.vocab) < 2:
-            return PredictionBatch(prediction_items)
-        else:
-            return MultiPredictionBatch(prediction_items)
 
 
 class HuggingFaceInferenceSession(InferenceSession):
